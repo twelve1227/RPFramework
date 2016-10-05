df3d628d3bb32e2977602921a18328a488d9d6ea
Ken Wong <kenwong@mb-mtvm103618.local>
2016-10-05 18:01:06 +0800

    Initial Commit

---
+++ RPFramework/RPFramework.h
@@ -0,0 +1,19 @@
+//
+//  RPFramework.h
+//  RPFramework
+//
+//  Created by Ken Wong on 5/10/2016.
+//  Copyright © 2016 Ken Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RPFramework.
+FOUNDATION_EXPORT double RPFrameworkVersionNumber;
+
+//! Project version string for RPFramework.
+FOUNDATION_EXPORT const unsigned char RPFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RPFramework/PublicHeader.h>
+
+
